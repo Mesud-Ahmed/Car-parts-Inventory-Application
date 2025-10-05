a0ab9ed4a710f3e4f4d3ec040323ae33639e3ac4
Mesud-Ahmed <mesudahmed94@gmail.com>
2025-10-05 14:29:05 +0300

    change .env variables

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const expressLayouts = require('express-ejs-layouts');
 const itemsRouter = require("./routes/items");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
